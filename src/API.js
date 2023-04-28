f0b8ce6af579bad9364e4a18edbd97c3da3df229
gharieb khalifa <blacksnow.soon@gmail.com>
2023-04-28 21:26:01 +0300

    adding the get projects and get project methods

--- src/API.js
+++ src/API.js
@@ -1,3 +1,5 @@
+import Projects from "./Components/Page/Projects";
+
 export async function getData() {
     const respons = await fetch("../data.json");
     return respons.json();
@@ -6,4 +8,16 @@ export async function getProduct(productName) {
     const respons = await fetch("../data.json");
     const { Products } = await respons.json().then(fullfilled => fullfilled);
     return Products[`${productName}`];
+}
+
+export async function getProjects(){
+    const respons = await fetch("../data.json");
+    const {Projects} = await respons.json().then(fullfilled => fullfilled)
+    return Projects;
+}
+export async function getProject(id) {
+    const respons = await fetch("../data.json")
+    const {Projects = {}} = await respons.json().then(fullfilled =>  fullfilled)
+    const project = Projects.filter(project => Number.parseInt(id) === project.id)
+    return project[0]
 }
