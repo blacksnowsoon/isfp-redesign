fa99afd1db9201fead15a94354a030ce19213297
gharieb khalifa <blacksnow.soon@gmail.com>
2023-04-28 21:27:11 +0300

    fixing the projects and project pathes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 
-import App, { loader as appLoader } from "./App";
+import App, { loader as appLoader, loader } from "./App";
 import {loader as productLoader } from './Components/Page/Product'
 import {
   createBrowserRouter,
@@ -12,6 +12,8 @@ import {
 import { Error } from "./Error";
 import Home from "./Components/Home/Home";
 import Product  from "./Components/Page/Product";
+import Projects, {loader as projectsLoader} from "./Components/Page/Projects";
+import Project, { loader as projectLoader} from "./Components/Page/Project"
 
 const router = createBrowserRouter([
   {
@@ -26,10 +28,25 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       {
-        path: "/products/*",
+        path: "products/*",
         element: <Product />,
         loader: productLoader,
       },
+      {
+        path: "projects",
+        loader: projectsLoader,
+        children: [
+          {
+            index: true,
+            element: <Projects />,
+          },
+          {
+            path: ":id",
+            element: <Project/>,
+            loader: projectLoader,
+          }
+        ]
+      }
     ],
   },
 ]);
