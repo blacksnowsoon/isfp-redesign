37ae9e208e3828c3ad47ebb9a4e080bfce49d3cd
gharieb khalifa <blacksnow.soon@gmail.com>
2023-05-01 00:43:10 +0300

    create a logic if it has a container or not

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -4,6 +4,7 @@ export const Image = (props) => {
   const path = '../assets/imgs/' + props?.url
   const src = new URL(path, import.meta.url).href
   return (
+    props.containerClassName ? 
     <div className={props.containerClassName}>
       <img 
         className={props?.className}
@@ -11,6 +12,11 @@ export const Image = (props) => {
         alt={props?.alt} 
         title={props?.title} />
     </div>
-    
+    :
+    <img 
+        className={props?.className}
+        src={src} 
+        alt={props?.alt} 
+        title={props?.title} />
   )
 }
