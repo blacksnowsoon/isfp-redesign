8e3d9e0ca9594200668d6e554bf7220043b3996f
gharieb khalifa <blacksnow.soon@gmail.com>
2023-05-10 02:29:20 +0300

    build the project page

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import { getProject } from "../API";
-
+import { Image } from "../Components/Image";
 const Project = () => {
-  const  { project = {} }  = useLoaderData();
+  const { project = {} } = useLoaderData();
   return (
-    <>
-    <h1>{project.id}</h1>
-    </>
-  
-  )
+    <section className="container project-page">
+      <Image
+        containerClassName={"project-page-thumb"}
+        url={`projects/${project.thumbnail}`}
+        alt={project.thumbnail.slice(-3)}
+        className={"pro-img"}
+      />
+      <div className="project-page-content">
+        <div>
+          <h3>{project.author}</h3>
+          <h2>{project.country}</h2>
+        </div>
+
+        <p>{project.overview}</p>
+        <p>{project.description}</p>
+      </div>
+    </section>
+  );
 };
 
-export default Project
+export default Project;
 
 export const loader = async (request) => {
   try {
     const project = await getProject(request.params.id);
     return { project };
-  } catch (err) {
-    
-  }
-  
+  } catch (err) {}
 };
