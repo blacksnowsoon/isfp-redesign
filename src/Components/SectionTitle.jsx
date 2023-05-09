1d5a5331ba646cd8ba60f6db23fc1154a9be9ce9
gharieb khalifa <blacksnow.soon@gmail.com>
2023-05-10 02:20:38 +0300

    adding an arialable attr to the h2 title

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 export const SectionTitle = (props) => {
   return (
     <h2 
-    className={props?.className} 
-    aria-label={props?.ariaLable}>
+      className={props?.className} 
+      aria-label={props?.ariaLable}
+    >
       {
         props?.caption
       }
