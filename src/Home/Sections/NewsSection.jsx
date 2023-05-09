ddb8bde08366b64e3b39e360a9ea10358d27541a
gharieb khalifa <blacksnow.soon@gmail.com>
2023-05-10 02:27:10 +0300

    build the news section

--- src/Home/Sections/NewsSection.jsx
+++ src/Home/Sections/NewsSection.jsx
@@ -1,37 +1,65 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import { SectionTitle } from '../../Components/SectionTitle'
 import { Image } from '../../Components/Image'
+import { Button } from '../../Components/Button'
 export const NewsSection = ({news}) => {
+  const listRef = useRef()
+
+  const handleExtendText = (e)=>{
+    const target = e.taget
+    const listFullHeight = listRef.current.scrollHeight
+    let listCurrentHeight = listRef.current.offsetHeight
+    if (listCurrentHeight < listFullHeight) {
+      listCurrentHeight += 500
+      listRef.current.style.height = listCurrentHeight+ "px"
+      if (listCurrentHeight >= listFullHeight) {
+        listRef.current.classList.toggle("shadow")
+      }
+    } else {
+      listRef.current.style.height = "var(--news-height)"
+      listRef.current.classList.toggle("shadow")
+    }
+    
+  }
+  useEffect(()=>{
+  }, [])
   return (
-    <section id='news&events' className='container'>
+    <section id='news&events' className='container news'>
       <SectionTitle className='sec-title' caption={"News & Events"} />
-      <ul className='news-wrapper'>
+      <ul ref={listRef} className='news-wrapper shadow'>
         {
-          news.map(event => {
-            return <EventSlide event={event} key={Math.random() + event.title.slice(0,6)}/>
+          news.map((event, index) => {
+          
+            return <EventSlide index={news.length-index} event={event} key={Math.random() + event.title.slice(0,6)}/>
           })
         }
+        
       </ul>
+      <div className='btn-wrapper'><Button caption={"+"} className={"btn-exetend"} event={handleExtendText} /></div>
     </section>
   )
 }
 
 
-const EventSlide = ({event}) => {
+const EventSlide = ({event, index}) => {
+  const style = {
+    zIndex : index
+  }
   return (
-    <li >
-      <div className='news-slide'>
+    <li className='news-card' style={style}>
+      <div className='thumbnail'>
+      <Image 
+        containerClassName={"img-container"} 
+        url={`news/${event.thumbnail}`} 
+        alt={event.title}/>
+      <small><i>In {event.date} </i>{event.title}</small>
+      </div>
+      
+      <div className='content'>
+        <h3>{event.title}</h3>
         
-        <Image 
-          containerClassName={"img"} 
-          url={`news/${event.thumbnail}`} 
-          alt={event.title}/>
-          
-        <div className='content'>
-          <h2>{event.title}</h2>
-          <i>In {event.date} </i>
-          <p>{event.overview}</p>
-        </div>
+        <p className='text-justify'>{event.overview}</p>
+        <p className='text-justify'>{event.subject}</p>
       </div>
     </li>
   )
